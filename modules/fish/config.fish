19ef8f50582f1a436a3031d27e0fd52ce08088f9
5n4wasa6 <kurosawa1991@gmail.com>
2020-02-25 20:10:09 +0900

    :+1: modify

--- modules/fish/config.fish
+++ modules/fish/config.fish
@@ -5,9 +5,9 @@ source ~/.config/fish/functions.fish
 # initialize
 function fish_prompt
     if [ $status -eq 0 ]
-        set status_face (set_color green)"(*´ω｀*) < "
+        set status_face (set_color green)">"(set_color yellow)">"(set_color red)"> "
     else
-        set status_face (set_color red)"｡+ﾟ(∩´﹏'∩)ﾟ+｡ < "
+        set status_face (set_color red)">>> "
     end
     printf '%s %s' (set_color yellow)(prompt_pwd) $status_face
 end
